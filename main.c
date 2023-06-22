7b59d6a759f441a7106116e25acbfc70faca1de6
omazoz <oumaymamazoz12@gmail.com>
2023-06-22 14:24:27 +0100

    done

--- main.c
+++ main.c
@@ -41,52 +41,53 @@ exit(EXIT_FAILURE);
 * Return: EXIT_SUCCESS if no errors or EXIT_FAILURE
 **/
 
-int main(__attribute__((unused)) int argc, char const *argv[]) {
-    FILE *mf;
-    char *buff = NULL, *opcode, *n;
-    int counter = 0;
-    stack_t *stack = NULL, *current;
+int main(__attribute__((unused)) int argc, char const *argv[])
+{
+FILE *mf;
+char *buff = NULL, *opcode, *n;
+int counter = 0;
+stack_t *stack = NULL, *current;
+if (argc != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+return EXIT_FAILURE;
+}
 
-    if (argc != 2) {
-        fprintf(stderr, "USAGE: monty file\n");
-        return EXIT_FAILURE;
-    }
+mf = fopen(argv[1], "r");
+if (mf == NULL) {
+fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+exit(1);
+}
 
-    mf = fopen(argv[1], "r");
-    if (mf == NULL) {
-        fprintf(stderr, "Error: can't open file %s\n", argv[1]);
-        exit(1);
-    }
+buff = malloc(MAX_LINE_LENGTH);
+if (buff == NULL) {
+fprintf(stderr, "Error: memory allocation failed\n");
+exit(1);
+}
 
-    buff = malloc(MAX_LINE_LENGTH);
-    if (buff == NULL) {
-        fprintf(stderr, "Error: memory allocation failed\n");
-        exit(1);
-    }
+while (fgets(buff, MAX_LINE_LENGTH, mf) != NULL) {
+counter++;
+opcode = strtok(buff, DELIMITER);
+if (opcode == NULL || opcode[0] == '#')
+continue;
+if (!strcmp(opcode, "nop"))
+continue;
+else if (!strcmp(opcode, "push")) {
+n = strtok(NULL, DELIMITER);
+push(&stack, n, counter);
+} else {
+find_opcode(&stack, opcode, counter);
+}
+}
 
-    while (fgets(buff, MAX_LINE_LENGTH, mf) != NULL) {
-        counter++;
-        opcode = strtok(buff, DELIMITER);
-        if (opcode == NULL || opcode[0] == '#')
-            continue;
-        if (!strcmp(opcode, "nop"))
-            continue;
-        else if (!strcmp(opcode, "push")) {
-            n = strtok(NULL, DELIMITER);
-            push(&stack, n, counter);
-        } else {
-            find_opcode(&stack, opcode, counter);
-        }
-    }
-
-    fclose(mf);
-    free(buff);
-    while (stack != NULL) {
-        current = stack;
-        stack = stack->next;
-        free(current);
-    }
-    return 0;
+fclose(mf);
+free(buff);
+while (stack != NULL) {
+current = stack;
+stack = stack->next;
+free(current);
+}
+return 0;
 }
 
 /**
